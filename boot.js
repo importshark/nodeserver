1cf3fd27ed2902bdc73cf99711fcba99b5f824db
importshark <87042591+importshark@users.noreply.github.com>
2021-10-18 18:27:07 -0400

    Update boot.js

--- boot.js
+++ boot.js
@@ -1,15 +1,19 @@
 var fs = require('fs');
 var path = require('path')
 var http = require('http');
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 var PORT = 3000;
 
 var staticServe = function(req, res) {
      if (req.url === "/") {
-        var stream = fs.createReadStream("assets/index.html");
+        var stream = fs.createReadStream("Contents/index.html");
   res.writeHead(200, {
     'Content-Type': 'text/html'
-  });
-
+  });	
+      
 
         stream.pipe(res);
 }
@@ -18,4 +22,8 @@ var staticServe = function(req, res) {
 var httpServer = http.createServer(staticServe);
 httpServer.listen(PORT);
 
+<<<<<<< Updated upstream
 console.log(`Server is running at PORT: ${PORT}`);
+=======
+console.log(`Server is running on PORT: ${PORT}`);
+>>>>>>> Stashed changes
